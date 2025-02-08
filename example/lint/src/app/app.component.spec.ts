7984295f88c5f1acd98bf9dd1be4e0c148466219
xuqj2 <xuqj2@lenovo.com>
2025-02-08 17:09:59 +0800

    add more stylistic lint rule

--- example/lint/src/app/app.component.spec.ts
+++ example/lint/src/app/app.component.spec.ts
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
         expect(app).toBeTruthy();
     });
 
-    it(`should have as title 'lint'`, () => {
+    it('should have as title \'lint\'', () => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.componentInstance;
         expect(app.title).toEqual('lint');
